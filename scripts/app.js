68d821a3f4d00b79c3cac35c7076e4f3fd186946
sukamichan <sukamichan@gmail.com>
2024-11-23 13:33:02 +0100

    Add files via upload
    travel website

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+var app = (function () {
+    /* Properties */
+    var websiteName = "TRAVEL-NOW";
+    /* Methods */
+    return {
+        getWebsiteName: function () {
+            return websiteName;
+        }
+    }
+}) ();
